541f35a15d82f12c7184fb87c0d4b49a53b4d9f0
eugenionoto <eugenio.noto@unige.ch>
2020-10-22 04:13:12 +0200

    searching and filtering

---
+++ app/models/db_search.rb
@@ -0,0 +1,42 @@
+class DbSearch
+	
+	MAX_RESULT_NUMBER = 30
+
+	def search_class
+		raise "missing class"
+	end
+	
+	def initialize(options = nil)
+		@options = options || {}
+	end
+	
+	def search
+		@results = search_class
+		apply_pagination
+		sort
+		filter
+		@results.all
+	end
+	
+	private
+	
+	def apply_pagination
+		if @options[:per_page].nil? || @options[:per_page] > MAX_RESULT_NUMBER
+			@options[:per_page] = MAX_RESULT_NUMBER
+		end
+		@options[:page] ||= 1
+		@results = @results.paginate(per_page: @options[:per_page], page: @options[:page])
+	end
+	
+	def filter
+		#no default filter
+	end
+	
+	def sort
+		#no default rule for order
+	end
+	
+	def validate_attribute(symbol)
+		search_class.attribute_names.include?(symbol.to_s)
+	end
+end
